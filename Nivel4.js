a946131b121494b87ac987ecbca74d183bab7f44
vinicius <vinicius.guimaraes@edu.senai.br>
2026-09-03 14:12:40 -0300

    Nivel 4

---
+++ Nivel4.js
@@ -0,0 +1,12 @@
+console.log('=== Exercícios: Métodos Arrays ===');
+
+// Exercício 1
+let frutas = ['Maçã', 'Banana', 'Laranja', 'Uva'];
+console.log(frutas.join());
+
+// Exercício 2
+console.log(frutas.join(" - "));
+
+// Exercício 3
+let palavras = ['JavaScript', 'é', 'muito', 'legal'];
+console.log(palavras.join(" "));
