941b9f5ad1ec095aac54dd9a98f3705f8abd66f0
vinicius <vinicius.guimaraes@edu.senai.br>
2026-09-03 13:45:12 -0300

    Nivel 2

---
+++ Nivel2.js
@@ -0,0 +1,18 @@
+console.log('=== Exercícios: Métodos Arrays ===');
+
+// Exercício 1
+let frutas = ['Maçã', 'Banana', 'Laranja', 'Uva', 'Manga'];
+console.log(frutas.slice(1, 4));
+
+// Exercício 2
+const nomes = ["Pam", "Paloma", "Gaby", "Duda", "Let", "Manu", "Kevin", "Thiago"];
+console.log(nomes.slice(0,3));
+
+// Exercício 3
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(numbers.slice(0, 3));
+
+// Exercício 4
+const aluno = ['Pam', 'Paloma', 'Gaby', 'Duda'];
+console.log(aluno.slice());
+console.log(aluno.slice(0, 2));
