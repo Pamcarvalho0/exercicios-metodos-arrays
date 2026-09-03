5bf52a5ad4fed001ef17e3d8302bc856a433c255
vinicius <vinicius.guimaraes@edu.senai.br>
2026-09-03 15:28:23 -0300

    Desafio Final

---
+++ Desafio.js
@@ -0,0 +1,12 @@
+console.log('=== Exercícios: Métodos Arrays ===');
+
+// DESAFIO FINAL
+
+const alunos = ['Pam', 'Manu', 'Let', 'Duda', 'Gaby', 'Paloma'];
+console.log('A Paloma existe?', alunos.includes('Paloma'));
+console.log('Qual a posição desse aluno na lista?', alunos.indexOf('Paloma'));
+console.log(alunos.slice());
+console.log(alunos.splice(1, 2));
+console.log(alunos.sort());
+console.log(alunos.reverse());
+console.log(alunos.join());
