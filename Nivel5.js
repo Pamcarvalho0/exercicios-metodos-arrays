de5176ff404160ecf2db5ec1214b9723c7dcbf6b
vinicius <vinicius.guimaraes@edu.senai.br>
2026-09-03 14:20:57 -0300

    Nivel 4

---
+++ Nivel5.js
@@ -0,0 +1,17 @@
+console.log('=== Exercícios: Métodos Arrays ===');
+
+// Exercício 1
+const aluno = ['Pam', 'Paloma', 'Gaby', 'Duda'];
+console.log(aluno.reverse());
+
+// Exercício 2
+let colocacoes = ['1º João', '2º Maria', '3º Pedro', '4º Ana'];
+console.log(colocacoes.reverse());
+
+// Exercício 3
+let nomes = ['Pam', 'Paloma', 'Gaby', 'Duda', 'Let', 'Manu', 'Kevin', 'Thiago'];
+console.log(nomes.sort());
+
+// Exercício 4
+let numeros = [50, 10, 100, 5, 25, 1];
+console.log(numeros.sort());
